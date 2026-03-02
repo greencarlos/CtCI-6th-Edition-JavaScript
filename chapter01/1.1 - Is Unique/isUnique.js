c4a3c1d9c7cd6b173ddbfd0e34daefe6797aa0ff
green.carlos <verdeclos@gmail.com>
2026-03-01 16:12:06 -0800

    1.1 IsUnique HashSet and Sorting solutions

--- chapter01/1.1 - Is Unique/isUnique.js
+++ chapter01/1.1 - Is Unique/isUnique.js
@@ -1,5 +1,4 @@
 var allUniqueChars = function(string) {
-  
   // O(n^2) approach, no additional data structures used
   // for each character, check remaining characters for duplicates
   for (var i = 0; i < string.length; i++) {
@@ -34,6 +33,44 @@ function everyCharUnique(str) {
   }
   return true;
 }
+
+/**
+ Sorting solution
+ Time O(n log n)
+ Space O(n) [for recursion]
+
+ We can also sort the input string as an array
+ Then iterate over the input string
+ If we find a duplicate character return false
+
+ Otherwise if we get through the entire string return true
+
+ **/
+
+function everyCharUnique(string) {
+  const str = string.split('').sort()
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === str[i - 1]) {
+      return false
+    }
+  }
+  return true
+}
+
+/**
+  HashSet Solution
+  Time & Space O(n)
+
+ The Hashing function can also be reduced to two lines
+ Utilizing a HashSet function, we only keep track of unique elements
+ Then compare the length / size of both string & set respectively
+ **/
+
+function everyCharUnique(string) {
+  const set = new Set(string.split(''))
+  return string.length === set.size
+}
+
 
 /* TESTS */
 console.log(everyCharUnique('abcd'), 'true');
